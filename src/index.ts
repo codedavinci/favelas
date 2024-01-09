4be0370a7901a847a03364f4182382a2eaa68e56
Eddie Silva <codedavinci.now@gmail.com>
2024-01-09 09:54:53 -0300

    feat(Package): Add Favelas class and all the underline methods

---
+++ src/index.ts
@@ -0,0 +1,57 @@
+type Favela = {
+  id: number;
+  name: string;
+  complex: string;
+  zone: string;
+  neighborhoods: string;
+};
+
+type State = "rj" | "sp";
+
+type Zone = "sul" | "oeste" | "central" | "norte";
+
+class Favelas {
+  state: State;
+  favelas: Favela[];
+
+  constructor(state: State = "rj") {
+    this.state = state;
+    this.favelas = require(`./data/${state}.json`);
+  }
+
+  getAll(): Favela[] {
+    return this.favelas;
+  }
+
+  getById(favelaId: number): Favela {
+    return this.favelas.find((favela) => favela.id === favelaId);
+  }
+
+  getAllNames(): string[] {
+    return this.favelas.map((favela) => favela.name);
+  }
+
+  getByName(name: string): Favela[] {
+    return this.favelas.filter((favela) => favela.name === name);
+  }
+
+  getFavelasByComplex(complexName: string): Favela[] {
+    return this.favelas.filter((favela) => favela.complex === complexName);
+  }
+
+  getFavelasByZone(zone: Zone): Favela[] {
+    return this.favelas.filter((favela) => favela.zone === zone);
+  }
+
+  getByNeighborhood(neighborhood: string): Favela[] {
+    return this.favelas.filter((favela) =>
+      favela.neighborhoods.includes(neighborhood)
+    );
+  }
+
+  total(): number {
+    return this.favelas.length;
+  }
+}
+
+export default Favelas;
